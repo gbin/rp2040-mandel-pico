26862356a96a6c644a2991102b288df26628d915
Guillaume Binet <gbin@gootz.net>
2022-06-27 12:07:15 +0900

    applied cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ use rp_pico::entry;
 // GPIO traits
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 
-
 // GPIO traits
 use embedded_hal::PwmPin;
 
@@ -43,32 +42,33 @@ use rp_pico::hal;
 use embedded_hal::spi::{MODE_0, MODE_1, MODE_3};
 use embedded_time::rate::*;
 
-use st7789::ST7789;
-use st7789::Orientation;
 use display_interface_spi::SPIInterface;
 use fixed::FixedI16;
-use rp_pico::hal::pio::PinState::Low;
 use mandel::MAX_MANDEL_ITERATION;
+use rp_pico::hal::pio::PinState::Low;
+use st7789::Orientation;
+use st7789::ST7789;
 
 use fixed::types::{I0F16, I16F0, I16F16, I9F7};
 type FP = I9F7;
 
-
-use embedded_graphics_core::pixelcolor::Rgb565;
+use crate::mandel::{GRAPHIC_BUFFER_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH};
 use embedded_graphics_core::draw_target::DrawTarget;
-use rp_pico::hal::{pac, Spi};
+use embedded_graphics_core::pixelcolor::Rgb565;
 use rp_pico::hal::gpio::Pins;
-use rp_pico::hal::sio::Sio;
 use rp_pico::hal::multicore::Multicore;
 use rp_pico::hal::multicore::Stack;
-use crate::mandel::{SCREEN_HEIGHT, SCREEN_WIDTH, GRAPHIC_BUFFER_SIZE };
+use rp_pico::hal::sio::Sio;
+use rp_pico::hal::{pac, Spi};
 
 const SCREEN_FREQUENCY_HZ: u32 = 125_000_000u32;
 const SCREEN_BAUDRATE: u32 = 16_000_000u32;
 
 /// entry point for second core
 static mut CORE1_STACK: Stack<4096> = Stack::new();
-fn core1_task() -> ! { loop{} }
+fn core1_task() -> ! {
+    loop {}
+}
 
 /// Entry point to our bare-metal application.
 ///
@@ -97,8 +97,8 @@ fn main() -> ! {
         &mut peripherals.RESETS,
         &mut watchdog,
     )
-        .ok()
-        .unwrap();
+    .ok()
+    .unwrap();
 
     // The single-cycle I/O block controls our GPIO pins
     let mut sio = hal::Sio::new(peripherals.SIO);
@@ -107,7 +107,7 @@ fn main() -> ! {
     let mut mc = Multicore::new(&mut peripherals.PSM, &mut peripherals.PPB, &mut sio.fifo);
     let cores = mc.cores();
     let core1 = &mut cores[1];
-    let _test = core1.spawn( unsafe { &mut CORE1_STACK.mem }, core1_task,);
+    let _test = core1.spawn(unsafe { &mut CORE1_STACK.mem }, core1_task);
     // end of multicore spawning
 
     // Set the pins up according to their function on this particular board
@@ -139,35 +139,58 @@ fn main() -> ! {
     // Our button input
     let button_pin = pins.gpio6.into_pull_up_input();
 
-    let spi = Spi::<_, _, 8>::new(peripherals.SPI0).init(&mut peripherals.RESETS, SCREEN_FREQUENCY_HZ.Hz(), SCREEN_BAUDRATE.Hz(), &MODE_0);
+    let spi = Spi::<_, _, 8>::new(peripherals.SPI0).init(
+        &mut peripherals.RESETS,
+        SCREEN_FREQUENCY_HZ.Hz(),
+        SCREEN_BAUDRATE.Hz(),
+        &MODE_0,
+    );
 
     let di = display_interface_spi::SPIInterface::new(spi, dc, cs);
 
-
-    let mut buffer:[u16; GRAPHIC_BUFFER_SIZE] = [0;GRAPHIC_BUFFER_SIZE];
+    let mut buffer: [u16; GRAPHIC_BUFFER_SIZE] = [0; GRAPHIC_BUFFER_SIZE];
     let mut lcd = ST7789::new(di, rst, SCREEN_HEIGHT as u16, SCREEN_WIDTH as u16);
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
     lcd.init(&mut delay);
-    lcd.clear(Rgb565::new(255, 0, 0) );
+    lcd.clear(Rgb565::new(255, 0, 0));
 
     let mut bx = I16F16::from_num(-2.00);
     let mut ex = I16F16::from_num(0.47);
     let mut by = I16F16::from_num(-1.12);
     let mut ey = I16F16::from_num(1.12);
     loop {
-        mandel::draw_on_buffer(bx, by, ex, ey, 0, 0, SCREEN_HEIGHT, SCREEN_WIDTH/2, &mut buffer);
-        mandel::draw_on_buffer(bx, by, ex, ey, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT, SCREEN_WIDTH, &mut buffer);
-        lcd.set_pixels(52, 40, SCREEN_HEIGHT + 51 , SCREEN_WIDTH +39, buffer);
-
+        mandel::draw_on_buffer(
+            bx,
+            by,
+            ex,
+            ey,
+            0,
+            0,
+            SCREEN_HEIGHT,
+            SCREEN_WIDTH / 2,
+            &mut buffer,
+        );
+        mandel::draw_on_buffer(
+            bx,
+            by,
+            ex,
+            ey,
+            0,
+            SCREEN_WIDTH / 2,
+            SCREEN_HEIGHT,
+            SCREEN_WIDTH,
+            &mut buffer,
+        );
+        lcd.set_pixels(52, 40, SCREEN_HEIGHT + 51, SCREEN_WIDTH + 39, buffer);
 
         // Run forever, setting the LED according to the button
         loop {
             if button_pin.is_low().unwrap() {
                 led_pin.set_high().unwrap();
-                bx = bx /2 + I16F16::from_num(0.2);
-                ex = ex /2 + I16F16::from_num(0.2);
-                by = by /2 + I16F16::from_num(0.2);
-                ey = ey /2 + I16F16::from_num(0.2);
+                bx = bx / 2 + I16F16::from_num(0.2);
+                ex = ex / 2 + I16F16::from_num(0.2);
+                by = by / 2 + I16F16::from_num(0.2);
+                ey = ey / 2 + I16F16::from_num(0.2);
                 break;
             } else {
                 led_pin.set_low().unwrap();
--- src/mandel.rs
+++ src/mandel.rs
@@ -1,19 +1,19 @@
-use fixed::types::I16F16;
 use crate::FP;
+use fixed::types::I16F16;
 
-pub const MAX_MANDEL_ITERATION:i32 = 100;
+pub const MAX_MANDEL_ITERATION: i32 = 100;
 
-pub const SCREEN_HEIGHT:u16 = 135;
-pub const SCREEN_WIDTH:u16 = 240;
-pub const GRAPHIC_BUFFER_SIZE:usize = (SCREEN_HEIGHT * SCREEN_WIDTH) as usize;
+pub const SCREEN_HEIGHT: u16 = 135;
+pub const SCREEN_WIDTH: u16 = 240;
+pub const GRAPHIC_BUFFER_SIZE: usize = (SCREEN_HEIGHT * SCREEN_WIDTH) as usize;
 
-pub fn mandel(x:f64, y:f64) -> i32 {
+pub fn mandel(x: f64, y: f64) -> i32 {
     let mut u = 0.0;
-    let mut v= 0.0;
+    let mut v = 0.0;
     let mut u2 = 0.0;
     let mut v2 = 0.0;
-    let mut k=0;
-    while k< MAX_MANDEL_ITERATION && (u2+v2<4.0){
+    let mut k = 0;
+    while k < MAX_MANDEL_ITERATION && (u2 + v2 < 4.0) {
         v = 2.0 * u * v + y;
         u = u2 - v2 + x;
         u2 = u * u;
@@ -23,14 +23,14 @@ pub fn mandel(x:f64, y:f64) -> i32 {
     k
 }
 
-pub fn mandel_fp<T:fixed::traits::Fixed>(x:T, y:T) -> i32 {
+pub fn mandel_fp<T: fixed::traits::Fixed>(x: T, y: T) -> i32 {
     let mut u = T::ZERO;
-    let mut v= T::ZERO;
+    let mut v = T::ZERO;
     let mut u2 = T::ZERO;
     let mut v2 = T::ZERO;
-    let mut iteration =0;
-    while iteration < MAX_MANDEL_ITERATION && (u2+v2)<(T::from_num(4.0)){
-        v = T::from_num(2.0 ) * u * v + y;
+    let mut iteration = 0;
+    while iteration < MAX_MANDEL_ITERATION && (u2 + v2) < (T::from_num(4.0)) {
+        v = T::from_num(2.0) * u * v + y;
         u = u2 - v2 + x;
         u2 = u * u;
         v2 = v * v;
@@ -52,12 +52,23 @@ pub fn mandel_fp<T:fixed::traits::Fixed>(x:T, y:T) -> i32 {
 /// pby = target pixel top right coord y
 /// pex = target pixel bottom left coord x
 /// pey = target pixel bottom right coord y
-pub fn draw_on_buffer(bx:I16F16, by:I16F16, ex:I16F16, ey:I16F16, pbx:u16, pby:u16, pex:u16, pey:u16, buffer: &mut [u16; GRAPHIC_BUFFER_SIZE]) {
+pub fn draw_on_buffer(
+    bx: I16F16,
+    by: I16F16,
+    ex: I16F16,
+    ey: I16F16,
+    pbx: u16,
+    pby: u16,
+    pex: u16,
+    pey: u16,
+    buffer: &mut [u16; GRAPHIC_BUFFER_SIZE],
+) {
     let mut color: u16;
     for py in pby..pey {
-        let x: I16F16 = ((ex -bx) / I16F16::from_num(SCREEN_WIDTH)) * I16F16::from_num(py) + bx;
+        let x: I16F16 = ((ex - bx) / I16F16::from_num(SCREEN_WIDTH)) * I16F16::from_num(py) + bx;
         for px in pbx..pex {
-            let y: I16F16 = ((ey - by) / I16F16::from_num(SCREEN_HEIGHT )) * I16F16::from_num(px) + by;
+            let y: I16F16 =
+                ((ey - by) / I16F16::from_num(SCREEN_HEIGHT)) * I16F16::from_num(px) + by;
             let iteration = mandel_fp(I16F16::to_num::<FP>(x), I16F16::to_num::<FP>(y));
             if iteration == MAX_MANDEL_ITERATION {
                 color = 0xffff;
